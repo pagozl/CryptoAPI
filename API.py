7cdc454c489220f982a8b469fc5b569a470aa863
Pawel <pgozlowski@gmail.com>
2020-02-22 20:27:11 +0100

    API 1.1

--- API.py
+++ API.py
@@ -5,14 +5,14 @@ response = request.urlopen('https://financialmodelingprep.com/api/v3/cryptocurre
 
 obj = json.load(response)
 crname = input("Podaj nazwe kryptowaluty (np. Bitcoin, Maker, Dogecoin): ")
-found=0
+found = 0
 
 for x in obj['cryptocurrenciesList']:
     if x['name'] == crname:
         print("Cena", crname, "to", x['price'], "USD")
-        found=1
+        found = 1
 
-if found==0:
+if found == 0:
     print("Niepoprawna nazwa kryptowaluty. Lista dostępnych kryptowalut:")
     for x in obj['cryptocurrenciesList']:
         print(x['name'])
